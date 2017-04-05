799f659954b4a2352612cad7dddeeabdfae3ecf1
mranftle <matthew.ranftle@gmail.com>
2017-04-05 17:51:26 -0500

    posterior plotted

--- gaussian_regression.m
+++ gaussian_regression.m
@@ -4,17 +4,10 @@ format short g
 % performs gaussian process regression with covSEiso kernel
 
 sample_colors = ...
-    [252, 146, 114; ...
-     251, 106,  74; ...
-     239,  59,  44; ...
-     203,  24,  29; ...
-     165,  15,  21] / 255;
- 
-% plot colors (from http://colorbrewer2.org/)
-colors = [ 31, 120, 180; ...
-           51, 160,  44; ...
-          227,  26,  28; ...
-          166, 206, 227] / 255;
+    [228,26,28; ...
+    55,126,184; ...
+    77,175,74; ...
+    152,78,1631] / 255;
 
 num_samples = 1;
 % get feature names for reference
@@ -36,27 +29,49 @@ training_total_updrs = training_data(:,6);
 test_total_updrs = test_data(:,6);
 
 % get relevent variables 
-% x = training_data(:,7:end);
 x = training_data(:,7:end);
 x_star = test_data(:,7:end);
-n_star = size(x,2);
+n = size(x,1);
+n_star = size(x_star,1);
+mu = mean(x);
+% noise 
+sigma = 0.2;
 
-%  prior
-% prior_mu = zeros(1,n_star);
-% prior_K = covZero(x);
-% samples = mvnrnd(prior_mu, prior_K, num_samples)';
+% parameters of K
+lambda      = 2;   % output scale
+ell         = 3;   % length scale
 
-% % K(X, X)
-Kxx = covSEiso(theta.cov,x);
-% K(X, X_*)
-Kxs = covSEiso(theta.cov,x,x_star);
-% K(X_*, K_*)
-Kss = covSEiso(theta.cov,x_star);
+theta.cov = [log(ell); log(lambda)];
 
-% posterior distribution for y*
-posterior_mu = Kxs' / Kxx * training_total_updrs;
-posterior_K  = Kss - Kxs' / Kxx * Kxs;
-posterior_K  = (posterior_K + posterior_K') / 2;
+% prior 
+prior_mu = zeros(1,n_star);
+prior_K = covSEiso(theta.cov,x_star);
 
-RMSE = sqrt(mean((posterior_mu - test_total_updrs).^2)); 
+
+%kernel 
+Kxx = covSEiso(theta.cov, x); 
+Kxs = covSEiso(theta.cov, x, x_star); 
+Kss = covSEiso(theta.cov, x_star); 
+
+V = Kxx + sigma^2*eye(n);
+
+posterior_mu = (Kxs'/V) * training_total_updrs;
+posterior_K = (Kss - Kxs'/V * Kxs) + eye(n_star)*2;
+
+RMSE = sqrt(mean((posterior_mu - test_total_updrs).^2));
 disp(RMSE);
+
+
+figure; 
+x_sig = linspace(0,n_star,n_star)';
+sigma_h = ...
+    fill([x_sig; flipud(x_sig)],...
+        [posterior_mu - 2*sqrt(diag(posterior_K)); ...
+         flipud(posterior_mu + 2 * sqrt(diag(posterior_K)))], ...
+         sample_colors(3,:), ...
+         'edgecolor','none',...
+         'facealpha',0.3);
+hold('on');
+mean_h = ...
+    plot(posterior_mu, ...
+         'color', sample_colors(1, :));
