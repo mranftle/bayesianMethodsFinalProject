976afe77073d751bae280416017560313c67c6cd
mranftle <matthew.ranftle@gmail.com>
2017-04-05 17:53:57 -0500

    clean up comments

--- gaussian_regression.m
+++ gaussian_regression.m
@@ -9,13 +9,10 @@ sample_colors = ...
     77,175,74; ...
     152,78,1631] / 255;
 
-num_samples = 1;
 % get feature names for reference
 data_file = fopen('data/parkinsons_data.csv', 'rt');
 features = fgetl(data_file);
 fclose(data_file);
-
-disp(features);
 
 % readcsv, split into training and test (5875,22)
 data = csvread('data/parkinsons_data.csv',1,0);
@@ -33,7 +30,7 @@ x = training_data(:,7:end);
 x_star = test_data(:,7:end);
 n = size(x,1);
 n_star = size(x_star,1);
-mu = mean(x);
+
 % noise 
 sigma = 0.2;
 
@@ -47,21 +44,22 @@ theta.cov = [log(ell); log(lambda)];
 prior_mu = zeros(1,n_star);
 prior_K = covSEiso(theta.cov,x_star);
 
-
 %kernel 
 Kxx = covSEiso(theta.cov, x); 
 Kxs = covSEiso(theta.cov, x, x_star); 
 Kss = covSEiso(theta.cov, x_star); 
 
+% get posterior
 V = Kxx + sigma^2*eye(n);
-
 posterior_mu = (Kxs'/V) * training_total_updrs;
 posterior_K = (Kss - Kxs'/V * Kxs) + eye(n_star)*2;
 
+% root mean squared error
 RMSE = sqrt(mean((posterior_mu - test_total_updrs).^2));
 disp(RMSE);
 
 
+% plot posterior_mu and posterior_K
 figure; 
 x_sig = linspace(0,n_star,n_star)';
 sigma_h = ...
